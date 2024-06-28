e671e6472197119bc6fe45b2aae4bf3efb5b8f10
Rakesh Kumar <rklsspty777@gmail.com>
2024-06-28 07:00:08 +0530

    setup: Create folder structure for C++ client
    Create folder structure for C++ client-side application for Windows platform. This commit includes README.md file in `docs/` folder. Also `.gitkeep` files are added in directories.
    
    Initially only a single CPP file inside of the `src/` directory is provided with `main` function which is the entrypoint of the application. It still needs modification but this commit ensures a proper project folder structure to be there.
    
    **Labels:** setup, documentation
    
    Fixes #6

---
+++ client/win/BluZoneLock-Win-Client/src/BluZoneLock-Win-Client.cpp
@@ -0,0 +1,20 @@
+/*
+* BluZoneLock-Win-Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+* This Bluetooth application runs in client mode only and operates within a console environment.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/**
+* @file BluZoneLock-Win-Client.cpp
+* @brief Bluetooth client application for BluZoneLock.
+* @details This console application initiates a Bluetooth connection in client mode.
+*
+* @author Rakesh Kumar
+* @date June 28, 2024
+*/
+int main() {
+    cout << "TODO\n" << endl;
+}
